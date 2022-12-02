61e29b467dffef5fec563a29f10a6292c8455ab4
Adharsh <adharshravisankar@gmail.com>
2022-12-01 21:31:31 -0800

    initial checkin

---
+++ src/trees/TreeException.java
@@ -0,0 +1,12 @@
+package trees;
+
+import java.io.*;
+
+public class TreeException extends Exception
+{
+    public TreeException(String s)
+    {
+        // Pass s into a superclass ctor.
+        super(s);
+    }
+}
